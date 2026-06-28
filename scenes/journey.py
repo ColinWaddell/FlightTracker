4a4fbbad66e4136540d0e25bfb20d6588693573a
Colin Waddell <mrcolin@gmail.com>
2026-06-28 12:12:35 +0100

    tweak font and positions

--- scenes/journey.py
+++ scenes/journey.py
@@ -275,8 +275,8 @@ class JourneyScene(object):
             origin_name = _abbreviate(origin_name)
             dest_name = _abbreviate(dest_name)
 
-        self._origin_name = origin_name or "unknown"
-        self._dest_name = dest_name or "unknown"
+        self._origin_name = origin_name or "Unknown"
+        self._dest_name = dest_name or "Unknown"
 
         for scroller, iata, name, arrow in (
             (self._origin_scroll, origin, self._origin_name, ">"),
