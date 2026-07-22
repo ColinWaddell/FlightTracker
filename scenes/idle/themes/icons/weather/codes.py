2c79508bafd80357f5947da43c650a6aa582fb8d
Colin Waddell <mrcolin@gmail.com>
2026-07-22 14:25:54 +0100

    make this easier to read

--- scenes/idle/themes/icons/weather/codes.py
+++ scenes/idle/themes/icons/weather/codes.py
@@ -1,124 +1,184 @@
 CODE_MAPPINGS = {
-    1000: ["sun", "moon", "rays", "moon_rays", 0],  # Sunny
-    1003: ["cloud_sun", "cloud_moon", "rays", "moon_rays", 1],  # Partly cloudy
-    1006: ["cloud", "cloud_moon", "", "", 0],  # Cloudy
-    1009: ["cloud_dark", "cloud_dark", "", "", 0],  # Overcast
-    1012: ["", "", "dusty", "dusty", 0],  # Haze
-    1015: ["", "", "dusty", "dusty", 0],  # Dust haze
-    1018: ["", "", "dusty", "dusty", 1],  # Blowing dust
-    1021: ["", "", "dusty", "dusty", 2],  # Dust storm
-    1024: ["", "", "dusty", "dusty", 2],  # Sandstorm
-    1027: ["", "", "dusty", "dusty", 2],  # Severe sandstorm
-    1030: ["", "", "mist", "mist", 0],  # Mist
-    1033: ["", "", "mist", "mist", 1],  # Smoke
-    1036: ["", "", "mist", "mist", 1],  # Smoky haze
-    1039: ["", "", "mist", "mist", 1],  # Smog
-    1042: ["", "", "mist", "mist", 2],  # Severe smog
-    1045: ["", "", "dusty", "dusty", 2],  # Saharan dust
-    1048: ["", "", "dusty", "dusty", 2],  # Dust
-    1063: ["cloud", "cloud_moon", "rain", "rain", 0],  # Patchy rain possible
-    1066: ["cloud", "cloud_moon", "snow", "snow", 0],  # Patchy snow possible
-    1069: ["cloud", "cloud_moon", "sleet", "sleet", 1],  # Patchy sleet possible
+    # Sunny
+    1000: ["sun", "moon", "rays", "moon_rays", 0],
+    # Partly cloudy
+    1003: ["cloud_sun", "cloud_moon", "rays", "moon_rays", 1],
+    # Cloudy
+    1006: ["cloud", "cloud_moon", "", "", 0],
+    # Overcast
+    1009: ["cloud_dark", "cloud_dark", "", "", 0],
+    # Haze
+    1012: ["", "", "dusty", "dusty", 0],
+    # Dust haze
+    1015: ["", "", "dusty", "dusty", 0],
+    # Blowing dust
+    1018: ["", "", "dusty", "dusty", 1],
+    # Dust storm
+    1021: ["", "", "dusty", "dusty", 2],
+    # Sandstorm
+    1024: ["", "", "dusty", "dusty", 2],
+    # Severe sandstorm
+    1027: ["", "", "dusty", "dusty", 2],
+    # Mist
+    1030: ["", "", "mist", "mist", 0],
+    # Smoke
+    1033: ["", "", "mist", "mist", 1],
+    # Smoky haze
+    1036: ["", "", "mist", "mist", 1],
+    # Smog
+    1039: ["", "", "mist", "mist", 1],
+    # Severe smog
+    1042: ["", "", "mist", "mist", 2],
+    # Saharan dust
+    1045: ["", "", "dusty", "dusty", 2],
+    # Dust
+    1048: ["", "", "dusty", "dusty", 2],
+    # Patchy rain possible
+    1063: ["cloud", "cloud_moon", "rain", "rain", 0],
+    # Patchy snow possible
+    1066: ["cloud", "cloud_moon", "snow", "snow", 0],
+    # Patchy sleet possible
+    1069: ["cloud", "cloud_moon", "sleet", "sleet", 1],
+    # Patchy freezing drizzle possible
     1072: [
         "cloud",
         "cloud_moon",
         "rain",
         "rain",
         0,
-    ],  # Patchy freezing drizzle possible
+    ],
+    # Thundery outbreaks possible
     1087: [
         "cloud",
         "cloud_moon",
         "thunder",
         "thunder",
         0,
-    ],  # Thundery outbreaks possible
-    1114: ["cloud", "cloud_moon", "blowing_snow", "blowing_snow", 1],  # Blowing snow
-    1117: ["cloud_dark", "cloud_dark", "snow", "snow", 2],  # Blizzard
-    1135: ["", "", "fog", "fog", 1],  # Fog
-    1147: ["", "", "fog", "fog", 1],  # Freezing fog
-    1150: ["cloud", "cloud_moon", "rain", "rain", 0],  # Patchy light drizzle
-    1153: ["cloud", "cloud_moon", "rain", "rain", 0],  # Light drizzle
-    1168: ["cloud", "cloud_dark", "rain", "rain", 1],  # Freezing drizzle
-    1171: ["cloud_dark", "cloud_dark", "rain", "rain", 2],  # Heavy freezing drizzle
-    1180: ["cloud", "cloud_moon", "rain", "rain", 0],  # Patchy light rain
-    1183: ["cloud", "cloud_moon", "rain", "rain", 0],  # Light rain
-    1186: ["cloud", "cloud_dark", "rain", "rain", 1],  # Moderate rain at times
-    1189: ["cloud", "cloud_dark", "rain", "rain", 1],  # Moderate rain
-    1192: ["cloud", "cloud_dark", "rain", "rain", 2],  # Heavy rain at times
-    1195: ["cloud_dark", "cloud_dark", "rain", "rain", 2],  # Heavy rain
-    1198: ["cloud", "cloud_moon", "rain", "rain", 0],  # Light freezing rain
+    ],
+    # Blowing snow
+    1114: ["cloud", "cloud_moon", "blowing_snow", "blowing_snow", 1],
+    # Blizzard
+    1117: ["cloud_dark", "cloud_dark", "snow", "snow", 2],
+    # Fog
+    1135: ["", "", "fog", "fog", 1],
+    # Freezing fog
+    1147: ["", "", "fog", "fog", 1],
+    # Patchy light drizzle
+    1150: ["cloud", "cloud_moon", "rain", "rain", 0],
+    # Light drizzle
+    1153: ["cloud", "cloud_moon", "rain", "rain", 0],
+    # Freezing drizzle
+    1168: ["cloud", "cloud_dark", "rain", "rain", 1],
+    # Heavy freezing drizzle
+    1171: ["cloud_dark", "cloud_dark", "rain", "rain", 2],
+    # Patchy light rain
+    1180: ["cloud", "cloud_moon", "rain", "rain", 0],
+    # Light rain
+    1183: ["cloud", "cloud_moon", "rain", "rain", 0],
+    # Moderate rain at times
+    1186: ["cloud", "cloud_dark", "rain", "rain", 1],
+    # Moderate rain
+    1189: ["cloud", "cloud_dark", "rain", "rain", 1],
+    # Heavy rain at times
+    1192: ["cloud", "cloud_dark", "rain", "rain", 2],
+    # Heavy rain
+    1195: ["cloud_dark", "cloud_dark", "rain", "rain", 2],
+    # Light freezing rain
+    1198: ["cloud", "cloud_moon", "rain", "rain", 0],
+    # Moderate or heavy freezing rain
     1201: [
         "cloud_dark",
         "cloud_dark",
         "rain",
         "rain",
         2,
-    ],  # Moderate or heavy freezing rain
-    1204: ["cloud", "cloud_moon", "sleet", "sleet", 0],  # Light sleet
-    1207: ["cloud_dark", "cloud_dark", "sleet", "sleet", 2],  # Moderate or heavy sleet
-    1210: ["cloud", "cloud_moon", "snow", "snow", 0],  # Patchy light snow
-    1213: ["cloud", "cloud_moon", "snow", "snow", 0],  # Light snow
-    1216: ["cloud", "cloud_dark", "snow", "snow", 1],  # Patchy moderate snow
-    1219: ["cloud", "cloud_dark", "snow", "snow", 1],  # Moderate snow
-    1222: ["cloud", "cloud_dark", "snow", "snow", 2],  # Patchy heavy snow
-    1225: ["cloud_dark", "cloud_dark", "snow", "snow", 2],  # Heavy snow
-    1237: ["cloud", "cloud_dark", "snow", "snow", 1],  # Ice pellets
-    1240: ["cloud", "cloud_moon", "rain", "rain", 0],  # Light rain shower
-    1243: ["cloud", "cloud_dark", "rain", "rain", 2],  # Moderate or heavy rain shower
-    1246: ["cloud_dark", "cloud_dark", "rain", "rain", 2],  # Torrential rain shower
-    1249: ["cloud", "cloud_moon", "sleet", "sleet", 0],  # Light sleet showers
+    ],
+    # Light sleet
+    1204: ["cloud", "cloud_moon", "sleet", "sleet", 0],
+    # Moderate or heavy sleet
+    1207: ["cloud_dark", "cloud_dark", "sleet", "sleet", 2],
+    # Patchy light snow
+    1210: ["cloud", "cloud_moon", "snow", "snow", 0],
+    # Light snow
+    1213: ["cloud", "cloud_moon", "snow", "snow", 0],
+    # Patchy moderate snow
+    1216: ["cloud", "cloud_dark", "snow", "snow", 1],
+    # Moderate snow
+    1219: ["cloud", "cloud_dark", "snow", "snow", 1],
+    # Patchy heavy snow
+    1222: ["cloud", "cloud_dark", "snow", "snow", 2],
+    # Heavy snow
+    1225: ["cloud_dark", "cloud_dark", "snow", "snow", 2],
+    # Ice pellets
+    1237: ["cloud", "cloud_dark", "snow", "snow", 1],
+    # Light rain shower
+    1240: ["cloud", "cloud_moon", "rain", "rain", 0],
+    # Moderate or heavy rain shower
+    1243: ["cloud", "cloud_dark", "rain", "rain", 2],
+    # Torrential rain shower
+    1246: ["cloud_dark", "cloud_dark", "rain", "rain", 2],
+    # Light sleet showers
+    1249: ["cloud", "cloud_moon", "sleet", "sleet", 0],
+    # Moderate or heavy sleet showers
     1252: [
         "cloud_dark",
         "cloud_dark",
         "sleet",
         "sleet",
         2,
-    ],  # Moderate or heavy sleet showers
-    1255: ["cloud", "cloud_moon", "snow", "snow", 0],  # Light snow showers
+    ],
+    # Light snow showers
+    1255: ["cloud", "cloud_moon", "snow", "snow", 0],
+    # Moderate or heavy snow showers
     1258: [
         "cloud_dark",
         "cloud_dark",
         "snow",
         "snow",
         2,
-    ],  # Moderate or heavy snow showers
-    1261: ["cloud", "cloud_moon", "snow", "snow", 0],  # Light showers of ice pellets
+    ],
+    # Light showers of ice pellets
+    1261: ["cloud", "cloud_moon", "snow", "snow", 0],
+    # Moderate or heavy showers of ice pellets
     1264: [
         "cloud_dark",
         "cloud_dark",
         "snow",
         "snow",
         2,
-    ],  # Moderate or heavy showers of ice pellets
+    ],
+    # Patchy light rain with thunder
     1273: [
         "cloud_sun",
         "cloud_moon",
         "thunder_rain",
         "thunder_rain",
         0,
-    ],  # Patchy light rain with thunder
+    ],
+    # Moderate or heavy rain with thunder
     1276: [
         "cloud_dark",
         "cloud_dark",
         "thunder_rain",
         "thunder_rain",
         2,
-    ],  # Moderate or heavy rain with thunder
+    ],
+    # Patchy light snow with thunder
     1279: [
         "snow_cloud",
         "snow_cloud",
         "thunder_snow",
         "thunder_snow",
         1,
-    ],  # Patchy light snow with thunder
+    ],
+    # Moderate or heavy snow with thunder
     1282: [
         "snow_cloud",
         "snow_cloud",
         "thunder_snow",
         "thunder_snow",
         2,
-    ],  # Moderate or heavy snow with thunder
+    ],
 }
 
 
